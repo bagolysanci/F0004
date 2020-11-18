e6e36f1e0dff6b56fc393097c1c1fca9f0cfb495
bagolysanci <bsanci04@gmail.com>
2020-11-18 20:14:49 +0000

    Initial commit

---
+++ f0004a.py
@@ -0,0 +1,5 @@
+kor = int(input('Hány éves vagy? '))
+ev = int(input('Milyen évet irunk? '))
+szuletes = ev-kor
+erettsegi = szuletes + 18
+print('Te' , erettsegi , 'ban fogsz érettségizni')
---
+++ f0004b.py
@@ -0,0 +1,3 @@
+szuletes = ev-kor
+erettsegi = szuletes + 18
+print('Te' + erettsegi + 'ban fogsz érettségizni')
